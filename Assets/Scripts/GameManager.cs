818460b6f141eca50a955925f10e5e9829254e2f
Dan Gorman <danthemangorman@gmail.com>
2017-10-02 14:36:49 -0400

    added function skeletons and built scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,26 @@ public class GameManager : MonoBehaviour {
 
     public static GameManager INSTANCE;
 
+    float chaos = 0;
+
+    [Header("Road Parents")]
+    public GameObject topRoad;
+    public GameObject middleRoad;
+    public GameObject bottomRoad;
+
+    House[] topHouses;
+    House[] middleHouses;
+    House[] bottomHouses;
+
+    float Chaos {
+        get {
+            return chaos;
+        }
+        set {
+            chaos = value;
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
 		if (INSTANCE != null) {
