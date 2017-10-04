ad73fdb9d0e25b42fb0189d73f333175c2ed8135
Dan Gorman <danthemangorman@gmail.com>
2017-10-04 14:45:19 -0400

    let there be shit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour {
     public GameObject thunderCloud;
     public Transform[] missleSpawns;
 
+    public Coroutine chaosRoutine;
+
     Person virus;
     float oZ;
 
@@ -44,6 +46,9 @@ public class GameManager : MonoBehaviour {
             }
             else if (superChaos == 5) {
                 StartCoroutine(RotateCamAroundZ());
+            } else if (superChaos == 15) {
+                StartCoroutine(ZoomAway());
+                StartCoroutine(ButtonToCenter());
             }
         }
     }
@@ -57,9 +62,9 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-	// Use this for initialization
-	void Awake () {
-		if (INSTANCE != null) {
+    // Use this for initialization
+    void Awake() {
+        if (INSTANCE != null) {
             this.enabled = false;
             return;
         }
@@ -67,12 +72,17 @@ public class GameManager : MonoBehaviour {
         people = new List<Person>();
         clouds = new List<Cloud>();
         cars = new List<Car>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
             Chaos = 0.99f;
+        } else if (Input.GetKeyDown(KeyCode.X)) {
+            chaosRoutine = null;
+            Chaos = 0;
+            SuperChaos = 0;
+            StopAllCoroutines();
         }
         if (Random.Range(0, 1.0f) > .994f) {
             SpawnCloud();
@@ -115,8 +125,8 @@ public class GameManager : MonoBehaviour {
 
     void SpawnCloud() {
         float x = Random.Range(-10.0f, 10.0f);
-        float y = Mathf.Sqrt(100.0f - (x * x))*(Random.Range(0, 1.0f) > .5f ? -1 : 1);
-        Instantiate(thunderCloud, new Vector3(x,y,0), Quaternion.Euler(0, 0, 0), transform);
+        float y = Mathf.Sqrt(100.0f - (x * x)) * (Random.Range(0, 1.0f) > .5f ? -1 : 1);
+        Instantiate(thunderCloud, new Vector3(x, y, 0), Quaternion.Euler(0, 0, 0), transform);
     }
 
     public Transform GetRandomMissleSpawn() {
@@ -128,10 +138,12 @@ public class GameManager : MonoBehaviour {
         if (Chaos < 1f) {
             Chaos += .01f;
         }
-        else if (virus == null && virusUI.transform.localScale==Vector3.zero) {
+        else if (virus == null && virusUI.transform.localScale == Vector3.zero) {
             Debug.Log("Sentience");
             virus = GetRandomPerson().BecomeSentient();
             people.Remove(virus);
+        } else if (chaosRoutine ==null){
+            chaosRoutine = StartCoroutine(GeneratePlayerChaos());
         }
         Debug.Log(Chaos);
     }
@@ -151,16 +163,17 @@ public class GameManager : MonoBehaviour {
     }
 
     IEnumerator GeneratePlayerChaos() {
+
         float t = 0;
         float jumpTime = 1f;
         while (t < jumpTime) {
             t += Time.deltaTime;
-            virusUI.transform.localPosition = new Vector3(0, Mathf.Sin(t * Mathf.PI)*30,0);
+            virusUI.transform.localPosition = new Vector3(0, Mathf.Sin(t * Mathf.PI) * 30, 0);
             yield return null;
         }
         chaosButton.onClick.Invoke();
         SuperChaos++;
-        yield return new WaitForSeconds(2*Mathf.PI-jumpTime);
+        yield return new WaitForSeconds(2 * Mathf.PI - jumpTime);
         StartCoroutine(GeneratePlayerChaos());
     }
 
@@ -171,7 +184,7 @@ public class GameManager : MonoBehaviour {
             float prev = t;
             t += Time.deltaTime;
             float cur = t;
-            cam.transform.position += transform.forward * (Mathf.Abs(Mathf.Sin(cur)) - Mathf.Abs(Mathf.Sin(prev)))*5;
+            cam.transform.position += transform.forward * (Mathf.Abs(Mathf.Sin(cur)) - Mathf.Abs(Mathf.Sin(prev))) * 5;
             yield return null;
         }
     }
@@ -186,6 +199,9 @@ public class GameManager : MonoBehaviour {
         }
         float t = 0;
         while (true) {
+            if (SuperChaos == 15) {
+                yield break;
+            }
             float prev = t;
             t += Time.deltaTime;
             float cur = t;
@@ -195,13 +211,34 @@ public class GameManager : MonoBehaviour {
     }
 
     IEnumerator RotateCamAroundZ() {
-        int dir = Random.Range(0,1f)>.5f? 1: -1;
+        int dir = Random.Range(0, 1f) > .5f ? 1 : -1;
         while (true) {
-            cam.transform.RotateAround(Vector3.zero, Vector3.up, Time.deltaTime * Random.Range(0,20f)*dir);
+            cam.transform.RotateAround(Vector3.zero, Vector3.up, Time.deltaTime * Random.Range(0, 20f) * dir);
             cam.transform.LookAt(Vector3.zero);
             if (Random.Range(0, 1.0f) > .994) {
                 dir *= -1;
             }
+            yield return null;
+        }
+    }
+
+    IEnumerator ZoomAway() {
+        while (true) {
+            cam.transform.position -= transform.forward * Time.deltaTime * 14.45f;
+            yield return null;
+        }
+    }
+
+    IEnumerator ButtonToCenter() {
+        float t = 0;
+        Vector3 position = chaosButton.transform.localPosition;
+        while (t < 10f) {
+            t += Time.deltaTime;
+            chaosButton.transform.localPosition = Vector3.Lerp(position, Vector3.zero, t / 10f);
+            yield return null;
+        }
+        while (true) {
+            chaosButton.transform.localPosition = Vector3.zero;
             yield return null;
         }
     }
